9f419e81bdab104bc18cefa65c9c6929fafe0d87
Fernando Wertt <fer19_reta@hotmail.com>
2020-10-08 21:46:11 -0500

    get products

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -12,7 +12,6 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class ProductService {
     constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
 
-    // Add product via firebase
     addProduct(menuLabels: MenuLabels) {
         this.http.post('https://ng-http-2b26c.firebaseio.com/arkus-menu.json', menuLabels)
         .subscribe(() => this.snackBar.open(menuLabels.producto + ' Added', 'Success', { duration: 2000, }));
@@ -20,15 +19,12 @@ export class ProductService {
 
     getProducts() {
         return this.http.get<{ [key: string ]: Post }>(
-            'https://ng-http-2b26c.firebaseio.com/menu.json')
+            'https://ng-http-2b26c.firebaseio.com/arkus-menu.json')
             .pipe(
                 map(responseData => {
                   const data: Post[] = [];
                   for (const key in responseData) {
                     if (responseData.hasOwnProperty(key)) {
-                      // {...responseData[key], id: key}
-                      // this is for add a new object
-                      // the , id: key is if we want to grab all the key and delete or move
                       data.push({...responseData[key], id: key});
                     }
                   }
